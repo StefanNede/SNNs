a6ddeb3da955fd1848f56a5b4170de61098c7446
Stefan Nedelcu <stefan.n@gmx.co.uk>
2026-02-20 17:50:05 +0000

    Changed folder structure and started work on final LIF and Synapse Collections vectorised + analytical collections

---
+++ LIFCollection.py
@@ -0,0 +1,12 @@
+# Vectorised collection of Analytical LIFs 
+import numpy as np
+
+class LIFCollection:
+    def __init__(self):
+        pass
+
+    def step(self, inputs):
+        pass
+
+    def reset(self):
+        pass
---
+++ SynapseCollection.py
@@ -0,0 +1,11 @@
+# Vectorised collection of analytical synapses
+
+class SynapseCollection:
+    def __init__(self):
+        pass
+
+    def step(self, inputs):
+        pass
+
+    def reset(self):
+        pass
